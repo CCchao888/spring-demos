932dba527c82c5886401af52e376d5b053f43b68
JinchaoWu <wjc510175817@163.com>
2024-12-24 22:03:04 +0800

    try git conflict 1

--- jpa-hibernate/src/main/java/com/chao/model/User.java
+++ jpa-hibernate/src/main/java/com/chao/model/User.java
@@ -1,25 +1,12 @@
 package com.chao.model;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.ConstraintMode;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.ForeignKey;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
-import java.util.List;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Builder
 @Data
@@ -51,13 +38,12 @@ public class User {
     //@Transient
     private List<Role> roles;
 
-    public void addRole(Role role)
-    {
+    public void addRole(Role role) {
         roles.add(role);
         role.getUsers().add(this);
     }
 
-    public void removeRole(Role role){
+    public void removeRole(Role role) {
         roles.remove(role);
         role.getUsers().remove(this);
     }
